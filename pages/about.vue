89baee955a27aa62140a74b05493e154c05cf1ca
Nicolae Cojuhari <nicojuhari@gmail.com>
2021-11-16 17:08:55 +0100

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<script setup>
+    console.time('setup')
+    const { data } = await useFetch('https://jsonplaceholder.typicode.com/posts')
+
+    console.timeEnd('setup')
+
+</script>
+
+<template>
+    <div>
+        <NuxtLink to="/">
+        About (internal link that belongs to the Nuxt App)
+        </NuxtLink>
+        <div v-for="item in data" :key="item.id">
+            {{ item.id}} : => {{ item.title }} 
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  Hello {{ name }} {{ jora }}
+    <br>
+    <NuxtLink to="/about">About</NuxtLink>
+</div>
+  
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('Nick 333')
+const jora = ref('jora')
+
+</script>
+
+<style>
+
+</style>
